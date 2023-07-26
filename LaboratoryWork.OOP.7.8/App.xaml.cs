8b4b0648ed5d62c4f9841a25eb6ee8b98105686c
Даниил Лямин <Niger120404@yandex.ru>
2023-07-26 17:19:08 +0300

    The framework has been changed

---
+++ LaboratoryWork.OOP.7.8/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace LaboratoryWork.OOP._7._8
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+	}
+}
---
+++ LaboratoryWork.OOP.7.8/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Data.Sqlite;
+using System.IO;
+using System.Net;
+using System.Windows;
+
+namespace LaboratoryWork.OOP._7._8
+{
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+		}
+
+		private void Button1_Click(object sender, RoutedEventArgs e)
+		{
+			string strokaNews = "https://news.yandex.ru/health.rss";
+			HttpWebRequest request_resusrs = (HttpWebRequest)WebRequest.Create(strokaNews);
+			using (HttpWebResponse response_resurs = (HttpWebResponse)request_resusrs.GetResponse())
+			{
+				using (Stream stream_resurs = response_resurs.GetResponseStream())
+				{
+					using (StreamReader streamReader = new StreamReader(stream_resurs))
+					{
+						using StreamReader reader_resurs = streamReader;
+						strokaNews = reader_resurs.ReadToEnd();
+					}
+					richTextBox1.Text = strokaNews;
+				}
+				response_resurs.Close();
+			}
+			button1.Content = "Готово";
+		}
+
+		private void Button2_Click(object sender, RoutedEventArgs e)
+		{
+			// Code to fetch news data and save it to the database using Entity Framework Core
+		}
+
+		private void Button3_Click(object sender, RoutedEventArgs e)
+		{
+			DeleteInfo();
+			_ = MessageBox.Show("Информация была удалена");
+		}
+
+		private void Button4_Click(object sender, RoutedEventArgs e)
+		{
+			using (SqliteConnection BD = new SqliteConnection("Data Source = TestMyBD.db;"))
+			{
+				BD.Open();
+				using SqliteCommand command = new SqliteCommand("DELETE FROM NEWS", BD);
+				_ = command.ExecuteNonQuery();
+			}
+			_ = MessageBox.Show("Инормация из БД была удалена");
+		}
+
+		public void DeleteInfo()
+		{
+			richTextBox1.Text = "";
+			richTextBox2.Text = "";
+			button1.Content = "Показать RSS";
+			button2.Content = "Показать новости";
+		}
+	}
+}
